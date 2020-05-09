47ae4794e960c064c0641326571e6a8db29e493c
Carlos Ibarra <carlosibarra.art@gmail.com>
2020-05-08 21:23:21 -0700

    built a sign-in component and set it in sign-in-sign-up component to view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInSignUpPage} />
       </Switch>
     </div>
   );
