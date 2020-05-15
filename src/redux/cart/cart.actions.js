72afdb796dfce3227336fe73899cfdec8d18475c
Carlos Ibarra <carlosibarra.art@gmail.com>
2020-05-15 16:16:35 -0700

    Updated shop data with new id-accurate  shop data, updated cart urils function to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,4 +3,10 @@ import { CartActionTypes } from './cart.types';
 export const toggleCartHidden = () => ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
-//export to cart.reducer.js and cart-icon.component.js
+//export to cart-icon.component.js
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+//export to collection-items.component.js
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,5 @@
 export const CartActionTypes = {
-  TOGGLE_CART_HIDDEN : 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 };
-
 //export to cart.reducer.js and cart.actions.js
